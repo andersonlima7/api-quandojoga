57b444ab28fac3ac964cfe233e8bc2cc778631c3
Anderson Lima <andersonlll007@gmail.com>
2023-05-10 15:17:52 -0300

    change crawler main seed

--- services/crawler.py
+++ services/crawler.py
@@ -1,187 +1,163 @@
 # bibliotecas
 import json
+import time
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-# import locale
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# try:
-#     locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
-# except:
-#     locale.setlocale(locale.LC_ALL, 'en_US')
 
+options = webdriver.ChromeOptions()
+options.add_argument('--headless') # Habilita o modo headless
+driver = webdriver.Chrome()
 
 
 today = datetime.now()
 matches = []
-seeds = []
+matches_seeds = []
+team_seeds = []
+league_seeds = ['https://onefootball.com/pt-br/competicao/brasileirao-serie-a-16/tabela']
+league = ''
+first_accept_cookie = True
 
-months_conversion = {
-    'Janeiro': 1,
-    'Fevereiro': 2,
-    'Março': 3,
-    'Abril': 4,
-    'Maio': 5,
-    'Junho': 6,
-    'Julho': 7,
-    'Agosto': 8,
-    'Setembro': 9,
-    'Outubro': 10,
-    'Novembro': 11,
-    'Dezembro': 12
-}
-
-
-def daysFromToday(date):
-    return (date - today).days + 1
 
 
 # Retorna os seeds para fazer o scrapping posteriormente.
-def getSeeds():
-
-    seed = "https://www.espn.com.br/futebol/times"
+def getTeamSeeds(seed):
 
     # Coleta a página.
     page = requests.get(seed)
 
     soup = BeautifulSoup(page.text, 'html.parser')
 
-
-    for link in soup.find_all('a', {"class" : "AnchorLink"}):
+    league = soup.find('p', {"class": "title-2-bold"}).contents[0]
+    for link in soup.find_all('a', {"class" : "standings__row-grid"}):
         href = link.get("href")
-        if "time" in href.split('/') and "calendario" in href.split('/'): 
-            new_seed = f"https://www.espn.com.br{href}"
-            if(new_seed not in seeds):
-                seeds.append(new_seed)
+        new_seed = f"https://onefootball.com{href}/jogos"
+        if(new_seed not in team_seeds):
+            team_seeds.append(new_seed)
 
-    return seeds
-
-
-# Coleta os dados de onde assistir os jogos.
-def scrappingTransmissions(date, team_home, team_away):
-
-    transmissions = []
-    formatted_date = date.strftime('%d-%m-%Y')
-    seed = f"https://www.uol.com.br/esporte/futebol/central-de-jogos/#/{formatted_date}"
-    page = requests.get(seed)
-    soup = BeautifulSoup(page.text, 'html.parser')
-
-    print(f"Searching for transmission info... \n Seed: " + seed)
-
-    match_wraps = soup.find_all('div', {"class" : "match-full match-wrapper"})
-    data = dict()
-    for match in match_wraps:
-
-        data_match = match.get('data-cfg')
-        data = json.loads(data_match)
-        date_match_string = data["data"].split(' ')[1]
-        date_match = f"{date_match_string}/{today.year}"
-
-
-        if(date_match != formatted_date.replace("-", "/")): continue
-
-
-        current_team_home = data["time1"].upper()
-        current_team_away = data["time2"].upper()
-
-        current_teams = [current_team_home, current_team_away]
-
-        if(team_home.upper() in current_teams or team_away.upper() in current_teams):
-            transmissions_container = match.find('div', {"class" : "transmitions"})
-
-            try:
-                transmissions_links = transmissions_container.findAll('a')
-                for link in transmissions_links:
-                    transmissions.append(link.contents[0])
-            except:
-                return [""]
-            print(transmissions)
-            return transmissions
+    return (sorted(team_seeds), league)
 
 
 
+def getMatchSeeds(team_seed):
 
+    driver.get(team_seed)
 
-# Coleta os dados dos times.
-def scrapping (seed) : 
-    team = {}
-    games_data = []
+    print('Starting')
 
-    # Coleta a primeira página.
-    page = requests.get(seed)
+    # Espera até que o elemento de aceitação de cookies seja carregado na página
+    global first_accept_cookie
+    if(first_accept_cookie):
+        wait = WebDriverWait(driver, 10)
+        cookie_banner = wait.until(EC.presence_of_element_located((By.ID, "onetrust-group-container")))
+        accept_button = cookie_banner.find_element(By.XPATH, "//button[@id='onetrust-reject-all-handler']")
+        accept_button.click()
+        load_matches_button = wait.until(EC.presence_of_element_located((By.XPATH, "//button[@class='of-button of-button--ghost']")))
+        load_matches_button.click()
+        first_accept_cookie = False
 
-    soup = BeautifulSoup(page.text, 'html.parser')
+    else:
+        # Espera até que o botão de carregamento de partidas seja carregado na página
+        load_matches_button = driver.find_element(By.XPATH, "//button[@class='of-button of-button--ghost']")
+        load_matches_button.click()
 
-
-
-    ID = seed.split('/')[8]
-
-    # Nome do time
-    team_name = soup.find('span', {"class": "db fw-bold"}).text.strip()
-
-    #Logo do time
-    team_logo = f"https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/{ID}.png&h=800&w=800"
-
-
-    print(f"Scrapping {team_name}...")
+    driver.implicitly_wait(10)
+    time.sleep(1.5)
 
     
-
-    # Pega todo texto da div que contém os jogos pelos meses.
-    months = soup.find_all(class_='ResponsiveTable Table__fixtures')
-
-    for month in months:
-        # Mês e ano
-        month_year = month.find(class_='Table__Title')
-        month_year_string = month_year.contents[0]
-        month_year_string_split = month_year_string.replace(" ", "").split(',')
-        month_date = month_year_string_split[0].title()
-        year_date = month_year_string_split[1]
-        
-
-        # Dados dos jogos
-        body = month.find(class_='Table__TBODY')
-        rows = body.find_all('tr')
-
-        for row in rows:
-            cells = row.find_all('td')
-            if len(cells) == 0:
-                continue
-            date = cells[0].text.strip().split(' ')[1]
-            team_home = cells[1].text.strip()
-            team_away = cells[3].text.strip()
-            time = cells[4].text.strip()
-            championship = cells[5].text.strip()
-            tv = ['']
+    
+    # except NoSuchElementException:
+    #     print("Não foi possível encontrar o botão de carregamento de partidas.")
+    # except ElementClickInterceptedException:
+    #     print("Não foi possível clicar no botão de carregamento de partidas.")
 
 
-            month_value = months_conversion[month_date]
-            date_str = f"{date} {month_value} {year_date}"
+    page = requests.get(team_seed)
 
-            date_obj = datetime.strptime(date_str, "%d %m %Y")
-            formatted_date = date_obj.strftime('%d-%m-%Y')
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-            # dias entre hoje e a data.
-            days = daysFromToday(date_obj)
-            
-            if(days <= 2):
-                tv = scrappingTransmissions(date_obj, team_home, team_away)
+    # soup = BeautifulSoup(team_seed, "html.parser")
 
-            games_data.append({"team_home":team_home, "team_away" :team_away, "date":formatted_date,"time":time, "championship": championship, "tv": tv})
-            
+    # team_name = soup.find('p', {"class": "title-2-bold"}).contents[0]
+    # team_logo = soup.find('picture', {"class": "of-image__picture"}).find('source').attrs['srcset']
 
-    team["name"] = team_name
-    team["logo"] = team_logo
-    team["matches"] = games_data
-    matches.append(team)
+    for link in soup.find_all('a', {"class" : "match-card"}):
+        href = link.get("href")
+        new_seed = f"https://onefootball.com{href}"
+        if(new_seed not in matches_seeds):
+            matches_seeds.append(new_seed)
+    return matches_seeds
+
+    
+# Coleta os dados das partidas.
+def scrapping (seed) : 
+    match = {}
+    games_data = []
+
+    # Coleta a página.
+    page = requests.get(seed)
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    print(f"Scrapping {seed}...")
 
 
-seeds = getSeeds()
-for seed in seeds:
-    scrapping(seed)
+    teams = soup.find_all(class_="MatchScoreTeam_container__tRx4x")
 
-# scrappingTransmissions(today, 'Remo', 'Corinthians')
+    try:
+        team_home_logo = teams[0].find('img', {"class": "EntityLogo_entityLogoImage___mo9D"}).attrs['src']
+        team_home_name = teams[0].find('span', {"class": "MatchScoreTeam_name__3KTX8"}).contents[0]
+        team_away_logo = teams[1].find('img', {"class": "EntityLogo_entityLogoImage___mo9D"}).attrs['src']
+        team_away_name = teams[1].find('span', {"class": "MatchScoreTeam_name__3KTX8"}).contents[0]
+        match_date = soup.find('time', {"class": "MatchScore_kickoffDate__39yHZ"}).find('span').contents[0]
+        match_time = soup.find('time', {"class": "MatchScore_kickoffTime__6Zixj"}).find('span').contents[0]
+        match_info = soup.find('ul', {"class": "MatchInfo_entries__lSpQD"}).find_all('li')
 
+        championship_info = match_info[0].find_all('span')
+        championship_logo = championship_info[0].find('img').attrs['src']
+        championship_name = championship_info[2].contents[0]
+
+        location = match_info[2].find_all('span')[2].contents[0]
+
+        size = len(match_info)
+        if(size == 4):
+            tv = match_info[3].find_all('span')[2].contents[0]
+        else:
+            tv = ''
+        match = {"team_home":team_home_name, "team_home_logo": team_home_logo, "team_away" :team_away_name, "team_away_logo": team_away_logo,"date":match_date,"time":match_time, "championship": championship_name, "championship_logo": championship_logo, "location": location, "tv": tv}
+
+    except:
+        print(f"Erro coleta da partida {seed}")
+    matches.append(match)
+
+
+
+for seed in league_seeds:
+    team_seeds, league = getTeamSeeds(seed)
+
+
+count = len(team_seeds)
+i = 0
+for team in team_seeds:
+    print(f"{i}/{count}")
+    matches_seeds = getMatchSeeds(team)
+    i+=1
+
+count = len(matches_seeds)
+i = 0
+for match in matches_seeds:
+    print(f"{i}/{count}")
+    scrapping(match)
+    i+=1
+
+
+print(matches)
 with open("./assets/json/matches.json", "w", encoding="utf-8") as outfile:
     json.dump(matches, outfile, ensure_ascii=False)
 
