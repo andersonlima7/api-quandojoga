836537fa48103bbf07ce9567607572db0920d351
andersonlima7 <andersonlll007@gmail.com>
2023-05-22 20:28:49 -0300

    res send 200

--- src/server.ts
+++ src/server.ts
@@ -17,43 +17,11 @@ app.register(championshipRoutes, {
   prefix: 'championship'
 });
 
-app.get('/', async () => {
-  const routes = {
-    matches: [
-      {
-        url: 'https://www-matches-api.onrender.com/matches',
-        description: 'Retorna todas as partidas'
-      },
-      {
-        url: 'https://www-matches-api.onrender.com/matches/year/month/day',
-        urlExample: 'https://www-matches-api.onrender.com/matches/23/04/15',
-        description: 'Retorna todas as partidas da data determinada'
-      },
-      {
-        url: 'https://www-matches-api.onrender.com/matches/:team',
-        urlExample: 'https://www-matches-api.onrender.com/matches/Bahia',
-        description: 'Retorna todas as partidas de um time'
-      },
-      {
-        url: 'https://www-matches-api.onrender.com/matches/championship/:championship',
-        urlExample:
-          'https://www-matches-api.onrender.com/matches/championship/Campeonato%20Brasileiro',
-        description: 'Retorna todas as partidas de um campeonato'
-      }
-    ],
-    teams: [
-      {
-        url: 'https://www-matches-api.onrender.com/teams',
-        description: 'Retorna todos os times cadastrados'
-      },
-      {
-        url: 'https://www-matches-api.onrender.com/teams/:name',
-        urlExample: 'https://www-matches-api.onrender.com/teams/Corinthians',
-        description: 'Retorna um time pelo nome'
-      }
-    ]
-  };
-  return routes;
+app.get('/', async function (request, reply) {
+  reply
+    .code(200)
+    .header('Content-Type', 'application/json; charset=utf-8')
+    .send({ hello: 'world' });
 });
 
 app.listen({ port: env.PORT }).then(() => console.log('HTTP Server Running!'));
