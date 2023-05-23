6d2a632ba26382dd801763d41f965b579a5568af
andersonlima7 <andersonlll007@gmail.com>
2023-05-22 22:01:03 -0300

    revert commit

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,15 @@ import { env } from './env';
 import { teamsRoutes } from './routes/teams';
 import { matchesRoutes } from './routes/matches';
 import { championshipRoutes } from './routes/championship';
+import cors from '@fastify/cors';
 
 const app = fastify();
 
-const port = env.PORT;
+app.register(cors, {
+  origin: '*', // ou '*', para permitir de qualquer origem
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+});
 
 app.register(teamsRoutes, {
   prefix: 'teams'
@@ -19,13 +24,49 @@ app.register(championshipRoutes, {
   prefix: 'championship'
 });
 
-app.get('/', async function (request, reply) {
-  reply
-    .code(200)
-    .header('Content-Type', 'application/json; charset=utf-8')
-    .send({ hello: 'world' });
+// Rota especial para listar todos os endpoints
+app.get('/endpoints', (req, res) => {
+  const routes = app.printRoutes();
+  res.send(routes);
 });
 
-app
-  .listen({ port: env.PORT })
-  .then(() => console.log(`Http server is running on ${port}`));
+app.get('/', async () => {
+  const routes = {
+    matches: [
+      {
+        url: 'https://www-matches-api.onrender.com/matches',
+        description: 'Retorna todas as partidas'
+      },
+      {
+        url: 'https://www-matches-api.onrender.com/matches/year/month/day',
+        urlExample: 'https://www-matches-api.onrender.com/matches/23/04/15',
+        description: 'Retorna todas as partidas da data determinada'
+      },
+      {
+        url: 'https://www-matches-api.onrender.com/matches/:team',
+        urlExample: 'https://www-matches-api.onrender.com/matches/Bahia',
+        description: 'Retorna todas as partidas de um time'
+      },
+      {
+        url: 'https://www-matches-api.onrender.com/matches/championship/:championship',
+        urlExample:
+          'https://www-matches-api.onrender.com/matches/championship/Campeonato%20Brasileiro',
+        description: 'Retorna todas as partidas de um campeonato'
+      }
+    ],
+    teams: [
+      {
+        url: 'https://www-matches-api.onrender.com/teams',
+        description: 'Retorna todos os times cadastrados'
+      },
+      {
+        url: 'https://www-matches-api.onrender.com/teams/:name',
+        urlExample: 'https://www-matches-api.onrender.com/teams/Corinthians',
+        description: 'Retorna um time pelo nome'
+      }
+    ]
+  };
+  return routes;
+});
+
+app.listen({ port: env.PORT }).then(() => console.log('HTTP Server Running!'));
