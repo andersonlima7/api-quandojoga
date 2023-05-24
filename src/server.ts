b6d93912a0082bff1d093f7ce11b7f42dd8b391b
Anderson Lima <andersonlll007@gmail.com>
2023-05-24 08:19:48 -0300

    add res messages on update

--- src/server.ts
+++ src/server.ts
@@ -30,13 +30,16 @@ app.get('/TJjto76ZDsUMtZX3GpKkud55', (req, res) => {
   exec('yarn populate', (error, stdout, stderr) => {
     if (error) {
       console.error(`Erro ao executar o comando: ${error.message}`);
+      res.status(400);
       return;
     }
     if (stderr) {
       console.error(`Erro de saída do comando: ${stderr}`);
+      res.status(400);
       return;
     }
     console.log(`Saída do comando: ${stdout}`);
+    res.status(200).send('Atualizando dados');
   });
 });
 
