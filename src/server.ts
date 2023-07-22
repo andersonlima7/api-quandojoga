cb9a42d33f7468f043191112e5a92afe9349a96f
Anderson Lima <andersonlll007@gmail.com>
2023-07-21 23:20:44 -0300

    delete wrong routes

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,11 @@ import { matchesRoutes } from './routes/matches';
 import { championshipRoutes } from './routes/championship';
 import cors from '@fastify/cors';
 
-import { exec } from 'child_process';
 
 const app = fastify();
 
 app.register(cors, {
-  origin: '*', // ou '*', para permitir de qualquer origem
+  origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 });
@@ -26,60 +25,5 @@ app.register(championshipRoutes, {
   prefix: 'championship'
 });
 
-// app.get('/TJjto76ZDsUMtZX3GpKkud55', (req, res) => {
-//   exec('yarn populate', (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`Erro ao executar o comando: ${error.message}`);
-//       res.status(400);
-//       return;
-//     }
-//     if (stderr) {
-//       console.error(`Erro de saída do comando: ${stderr}`);
-//       res.status(400);
-//       return;
-//     }
-//     console.log(`Saída do comando: ${stdout}`);
-//     res.status(200).send('Atualizando dados');
-//   });
-// });
-
-app.get('/', async () => {
-  const routes = {
-    matches: [
-      {
-        url: 'https://www-matches-api.onrender.com/matches',
-        description: 'Retorna todas as partidas'
-      },
-      {
-        url: 'https://www-matches-api.onrender.com/matches/year/month/day',
-        urlExample: 'https://www-matches-api.onrender.com/matches/23/04/15',
-        description: 'Retorna todas as partidas da data determinada'
-      },
-      {
-        url: 'https://www-matches-api.onrender.com/matches/:team',
-        urlExample: 'https://www-matches-api.onrender.com/matches/Bahia',
-        description: 'Retorna todas as partidas de um time'
-      },
-      {
-        url: 'https://www-matches-api.onrender.com/matches/championship/:championship',
-        urlExample:
-          'https://www-matches-api.onrender.com/matches/championship/Campeonato%20Brasileiro',
-        description: 'Retorna todas as partidas de um campeonato'
-      }
-    ],
-    teams: [
-      {
-        url: 'https://www-matches-api.onrender.com/teams',
-        description: 'Retorna todos os times cadastrados'
-      },
-      {
-        url: 'https://www-matches-api.onrender.com/teams/:name',
-        urlExample: 'https://www-matches-api.onrender.com/teams/Corinthians',
-        description: 'Retorna um time pelo nome'
-      }
-    ]
-  };
-  return routes;
-});
 
 app.listen({ port: env.PORT }).then(() => console.log('HTTP Server Running!'));
